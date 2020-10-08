ea7cb69832ffe0d719febdc11e3ca45289f7a39a
thaerachman <71365565+thaerachman@users.noreply.github.com>
2020-10-08 12:15:00 +0700

    Add files via upload

---
+++ Tugas_IF_Perbandingan.php
@@ -0,0 +1,16 @@
+<?php
+$a =  7;
+$b =  5.5;
+
+	if($a < $b) {
+		echo "Nilai $a lebih kecil dari Nilai $b";
+	}else{
+		if($a > $b){
+			echo "Nilai $a Lebih besar dari Nilai $b";
+		}else{
+			echo "Nilai $a sama dengan Nilai $b";
+		}
+		
+	}
+
+?>
